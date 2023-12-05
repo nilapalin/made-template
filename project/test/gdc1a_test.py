da1423472b203e41ac9dbf5e493e1c91fad33e3a
nilapalin <agnesthanbichler@web.de>
2023-12-05 23:31:28 +0100

    Component test for gdc1a data 'requestPost'

--- project/test/gdc1a_test.py
+++ project/test/gdc1a_test.py
@@ -1,17 +1,20 @@
+import json
 import os
 import sqlite3
 import unittest
+from unittest.mock import MagicMock, patch
+from urllib import request
 
-from data.gdc1a import runPipeline
+from data.gdc1a import Gdc1aPipeline
 
 class Test_gdc1a(unittest.TestCase):
     def setUp(self):
-        test = 1
+        self.classUnderTest = Gdc1aPipeline()
 
-    def test_pipeline(self):
+    def test_system_pipeline(self):
         #arrange
         #act
-        total, path = runPipeline()
+        total, path = self.classUnderTest.runPipeline()
         #assert
         self.assertTrue(os.path.exists(path))
         con = sqlite3.connect(path)
@@ -19,3 +22,24 @@ class Test_gdc1a(unittest.TestCase):
         actual_total = cursor.fetchone()[0]
         self.assertEqual(total, actual_total, "The number of cases in the database {} is not the downloaded sample number {}".format(actual_total, total))
         con.close()
+
+    @patch('urllib.request.urlopen')
+    @patch('urllib.request.Request')
+    def test_component_request_post(self, mock_urlopen, mock_request):
+        #arrange
+        expected_json = "{name='test', weather='sunny'}"
+        expected_json_encoded = expected_json.encode("UTF-8")
+        mock_urlopen.return_value = MagicMock()
+        mock_urlopen.reason.return_value = 'OK'
+        mock_urlopen.return_value.__enter__.return_value.reason = 'OK'
+        mock_urlopen.read.return_value = expected_json_encoded
+        mock_urlopen.return_value.__enter__.return_value.read.return_value = expected_json_encoded
+        mock_request.return_value = ""
+        request.Request = mock_request
+        request.urlopen = mock_urlopen
+        #act
+        jsonResponse = self.classUnderTest.requestPost(url="testurl", data={"query": "", 'variables': ""})
+        #assert
+        self.assertEqual(jsonResponse, json.dumps(expected_json, indent=2))
+
+        
